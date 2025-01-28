072576f9a3d0514618800f0460f8c5e267293ddb
KeKs0r <marc.hoeffl@laserfocus.io>
2025-01-28 16:27:12 +0530

    ♻️ Refactor private endpoint check to use OpenAPI types

--- scripts/src/check/check-private.ts
+++ scripts/src/check/check-private.ts
@@ -1,3 +1,18 @@
-export async function checkPrivate() {
-  console.log("CHECK TRUE");
+import { OpenAPIV3 } from "openapi-types";
+
+export function verifyNoPrivateEndpoints(
+  paths: OpenAPIV3.PathsObject
+): boolean {
+  for (const route in paths) {
+    for (const method in paths[route]) {
+      const operation = paths[route][method];
+
+      if (operation["x-private"]) {
+        throw new Error(`Private endpoint found: ${route} ${method}`);
+      }
+    }
+  }
+
+  console.log("No private endpoints found.");
+  return true;
 }
