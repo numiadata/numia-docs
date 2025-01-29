c48f0e6a113dbf7326769c232bbdfde20db16edb
Marc Höffl <marc.hoeffl@laserfocus.io>
2025-01-29 15:36:50 +0100

    Marc/num 4134 osmosis docs implement better grouping on the endpoints (#32)
    * refactor: Remove deprecated OAS properties script and clean up debug logging
    
    * Stash
    
    * Update generation logic and folders
    
    * stash changes
    
    * Clean slate
    
    * refactor: Reorganize OAS generation and transformation scripts
    
    Move OAS-related scripts to a more structured `cmd` directory, update import paths, and modify script entry points to improve code organization and maintainability
    
    * Generate api menu default
    
    * Clean up and generate only osmosis api reference
    
    * Make it easier, one oas file per section
    
    * grouping and a lot of scripts, phew
    
    * 🔧 Update CI workflow paths and commands
    
    Refactored the paths and commands in the CI workflow files to align with the new directory structure. This includes updating the paths for running checks, downloading OAS files, transforming OAS files, and generating API documentation.
    
    * 🔧 Update script paths in package.json
    
    Modified the script paths in package.json to reflect the new directory structure for command files.

---
+++ scripts/src/oas-generation/download-oas.ts
@@ -0,0 +1,37 @@
+import { writeFileSync, rmSync, mkdirSync, readFileSync } from "fs";
+import Path from "path";
+import { apiConfig } from "../api-config";
+
+const REPOSITORY_ROOT = Path.join(__dirname, "../../../");
+export const OAS_ROOT_DIR = Path.join(REPOSITORY_ROOT, "openAPI");
+const CONFIG_FILE_PATH = Path.join(REPOSITORY_ROOT, "config.json");
+
+export async function rebuildOas() {
+  cleanOasDir();
+  await downloadOasFiles();
+  updateApiFilesInConfig();
+}
+
+function cleanOasDir() {
+  rmSync(OAS_ROOT_DIR, { recursive: true, force: true });
+  mkdirSync(OAS_ROOT_DIR, { recursive: true });
+}
+
+async function downloadOasFiles() {
+  return await Promise.all(
+    apiConfig.map(async (apiSection) => {
+      const response = await fetch(apiSection.oasFile);
+      const body = await response.json();
+      writeFileSync(
+        Path.join(OAS_ROOT_DIR, `${apiSection.name}.json`),
+        JSON.stringify(body, null, 2)
+      );
+    })
+  );
+}
+
+function updateApiFilesInConfig() {
+  const config = JSON.parse(readFileSync(CONFIG_FILE_PATH, "utf-8"));
+  config.apiFiles = apiConfig.flatMap((c) => `${c.name}.json`);
+  writeFileSync(CONFIG_FILE_PATH, JSON.stringify(config, null, 2));
+}
